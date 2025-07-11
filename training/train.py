8067773b910c99298a3e139e7f1bc72225a9d98e
yared-shewarade <yared.shewarade@gmail.com>
2025-07-11 00:58:57 -0700

    updated train

--- training/train.py
+++ training/train.py
@@ -16,3 +16,17 @@ optimizer = optim.Adam(model.parameters(), lr=le - 4)
 
 train_dataset = DummyVLCommandDataset(num_samples=100)
 train_loader = DataLoader(train_dataset, batch - size=BATCH_SIZE, shuffle=True)
+
+for epoch in range(EPOCHS):
+    model.train() 
+    running_loss = 0.0 
+    for batch in train_loader:
+        images, text_embeds, labels = [x.to(DEVICE) for x in batch]
+        optimizer.zero_grad() 
+        outputs = model(images, text_embeds)
+        loss = criterion(outputs, labels)
+        loss.backward() 
+        optimizer.step() 
+        running_loss += loss.item() 
+    print(f"Epoch {epoch + 1}/{EPOCHS}, Loss: {running_loss:.4f}")
+print("Training complete.")
